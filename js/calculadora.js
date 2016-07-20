8cb6edc4939fdae9ce77d73959f6c93e3824b585
Santiago <smartnix@hotmail.com>
2016-07-19 21:30:21 -0500

    javascript para calculadora y lista

---
+++ js/calculadora.js
@@ -0,0 +1,27 @@
+var num1 = "", num2="";
+var num = true;
+var op;
+
+function addNumber(n) {
+    
+    if(ban){
+        num1 +=n;
+    }
+    else{
+        num2 +=n
+    }
+   
+}
+
+function operador(op){
+    this.num = false;
+
+    var ret;
+    switch (op) {
+        case "-":ret = -;break;
+    
+        default:
+            break;
+    }
+
+}
